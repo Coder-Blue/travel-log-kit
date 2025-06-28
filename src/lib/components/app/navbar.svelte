1a364c121551476459146807c08de5dd09d609c7
Noah Trần <trananhquan1009@gmail.com>
2025-06-28 23:55:16 +0700

    28/06/25 - add landing page (#50)

---
+++ src/lib/components/app/navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Github } from "@lucide/svelte";
+</script>
+
+<div class="navbar bg-primary text-primary-content">
+  <div class="navbar-start">
+    <a href="/" class="btn btn-ghost text-xl">
+      Travel Log
+    </a>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-accent">Đăng nhập với Github<Github size="24" /></button>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  import Navbar from "$lib/components/app/navbar.svelte";
+  
   import "../app.css";
 
   const { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+  <Navbar />
+  <main>
+    {@render children()}
+  </main>
+</div>
