65a9487acf18313798946a169dbe4ddd0519c333
Coder-Blue <trananhquan1009@gmail.com>
2025-06-29 00:15:08 +0700

    29/06/25 add landing page / basic functions

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Navbar from "$lib/components/app/navbar.svelte";
+
+  import "../app.css";
+
+  const { children } = $props();
+</script>
+
+<div class="flex min-h-screen flex-col">
+  <Navbar />
+  <main>
+    {@render children()}
+  </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Github } from "@lucide/svelte";
+</script>
+
+<div class="hero bg-base-300 container mx-auto mt-4">
+  <div class="hero-content text-center min-h-96">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Nhật kí du lịch</h1>
+      <p class="py-6">
+        Theo dõi các chuyến đi và cuộc phiêu lưu của bạn bằng ứng dụng nhật ký du lịch đơn giản này. Thêm địa điểm, ảnh và ghi chú để tạo nhật ký kỹ thuật số về hành trình của bạn.
+      </p>
+      <button class="btn btn-accent">Đăng nhập với Github<Github size="24" /></button>
+    </div>
+  </div>
+</div>
