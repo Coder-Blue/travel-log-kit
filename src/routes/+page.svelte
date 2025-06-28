6e371334b55f415876dbedd476f3532314bd352a
Coder-Blue <trananhquan1009@gmail.com>
2025-06-28 23:45:51 +0700

    28/06/25 - add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-  import { Map } from "@lucide/svelte";
+  import { Github } from "@lucide/svelte";
 </script>
 
-<button class="btn btn-secondary">
-  Check <Map />
-</button>
+<div class="hero bg-base-300 container mx-auto mt-4">
+  <div class="hero-content text-center min-h-96">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Nhật kí du lịch</h1>
+      <p class="py-6">
+        Theo dõi các chuyến đi và cuộc phiêu lưu của bạn bằng ứng dụng nhật ký du lịch đơn giản này. Thêm địa điểm, ảnh và ghi chú để tạo nhật ký kỹ thuật số về hành trình của bạn.
+      </p>
+      <button class="btn btn-accent">Đăng nhập với Github<Github size="24" /></button>
+    </div>
+  </div>
+</div>
