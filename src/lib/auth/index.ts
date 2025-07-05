0ffa1435fbab010cdb08737f2aa8f9c2878c15e9
Coder-Blue <trananhquan1009@gmail.com>
2025-07-05 13:57:15 +0700

    05/07/25 - add auth middleware for ssr

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,13 +1,33 @@
+import type { User } from "better-auth";
+
 import { env } from "$env/dynamic/private";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createAuthMiddleware } from "better-auth/plugins";
 
 import db from "../db";
+
+export type UserWithId = Omit<User, "id"> & {
+  id: number;
+};
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "/get-session") {
+        if (!ctx.context.session) {
+          return ctx.json({
+            session: null,
+            user: null,
+          });
+        }
+        return ctx.json(ctx.context.session);
+      }
+    }),
+  },
   advanced: {
     generateId: false,
   },
