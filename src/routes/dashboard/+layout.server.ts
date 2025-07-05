1997a7161b51ebef1409f5c315dc7889897ed349
Coder-Blue <trananhquan1009@gmail.com>
2025-07-05 12:28:01 +0700

    05/07/25 - add protected route / modify homepage

---
+++ src/routes/dashboard/+layout.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from "@sveltejs/kit";
+import { auth } from "$lib/auth";
+
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ request }) => {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  if (!session) {
+    redirect(302, "/");
+  }
+};
